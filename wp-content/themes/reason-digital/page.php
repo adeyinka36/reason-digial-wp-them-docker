df0372b5a0b140100d56ec740541f70c7114c782
Adeyinka Giwa <yinkadev1992@gmail.com>
2022-01-30 11:11:06 +0000

    added styles completed main section

--- wp-content/themes/reason-digital/page.php
+++ wp-content/themes/reason-digital/page.php
@@ -1,7 +1,33 @@
 
 <?php
  get_header();
+?>
+<main>
+    <div class="top-background">
+        <h2>Get online week 2021</h2>
+        <p>Lorem ipsum dolor,  officia nihil iure voluptates! Ad cum odit repellendus laudantium, nesciunt sapiente voluptatibus at enim. Doloribus, tempora nostrum. Nisi, odio!</p>
+        <span>Get involved now!</span>   
+    </div>
+    <div class="bottom-background">
+        <div class="left">
+        </div>
+        <div class="right top-background">
+            <h2>We are a special charity helping people to change thier lives</h2>
+            <p>Lorem ipsum dolor,  officia nihil iure voluptates! Ad cum odit repellendus laudantium, nesciunt sapiente voluptatibus at enim. Doloribus, tempora nostrum. Nisi, odio!</p>
+            <span>learn more now!</span>
+            <i class="fas fa-fingerprint"></i>
+        </div>    
+    </div>    
+</main>
+<section>
+    <h2>How can we help you?</p>
+    <p>Lorem ipsum dolor sit,  soluta nesciunt magni velit deleniti doloribus at veniam.</p>
+    <span>I am <input type="dropdown"></input> and I want <input type="dropdown"> <button>start now</button>
+</section>
 
- 
+ <?php
  get_footer();
  ?>
+ <style>
+    
+</style>
